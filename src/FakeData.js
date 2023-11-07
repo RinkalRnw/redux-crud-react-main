fbb5358e522759026c4a56a05624b3a385be17d6
Smitkevadiya <f370smit@gmail.com>
2023-11-07 10:08:17 +0530

    react-redux projects

---
+++ src/FakeData.js
@@ -0,0 +1,12 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+  }
+];
